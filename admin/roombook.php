670049c0502caa4e1288f17b32a3203cf1395842
QuangAnh17920 <67898698+QuangAnh17920@users.noreply.github.com>
2021-11-04 03:00:39 +0700

    fixed roombook.php

--- admin/roombook.php
+++ admin/roombook.php
@@ -257,3 +257,325 @@ if(!isset($_SESSION["user"]))
                         </div>
                     </div>
 		    </div>
+			
+			
+                    <?php
+						$rsql ="select * from room";
+						$rre= mysqli_query($con,$rsql);
+						$r =0 ;
+						$sc =0;
+						$gh = 0;
+						$sr = 0;
+						$dr = 0;
+						while($rrow=mysqli_fetch_array($rre))
+						{
+							$r = $r + 1;
+							$s = $rrow['type'];
+							$p = $rrow['place'];
+							if($s=="Superior Room" )
+							{
+								$sc = $sc+ 1;
+							}
+							
+							if($s=="Guest House")
+							{
+								$gh = $gh + 1;
+							}
+							if($s=="Single Room" )
+							{
+								$sr = $sr + 1;
+							}
+							if($s=="Deluxe Room" )
+							{
+								$dr = $dr + 1;
+							}
+							
+						
+						}
+						?>
+						
+						<?php
+						$csql ="select * from payment";
+						$cre= mysqli_query($con,$csql);
+						$cr =0 ;
+						$csc =0;
+						$cgh = 0;
+						$csr = 0;
+						$cdr = 0;
+						while($crow=mysqli_fetch_array($cre))
+						{
+							$cr = $cr + 1;
+							$cs = $crow['troom'];
+							
+							if($cs=="Superior Room"  )
+							{
+								$csc = $csc + 1;
+							}
+							
+							if($cs=="Guest House" )
+							{
+								$cgh = $cgh + 1;
+							}
+							if($cs=="Single Room" )
+							{
+								$csr = $csr + 1;
+							}
+							if($cs=="Deluxe Room" )
+							{
+								$cdr = $cdr + 1;
+							}
+							
+						
+						}
+				
+					?>
+					<div class="col-md-4 col-sm-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                           Available Room Details
+                        </div>
+                        <div class="panel-body">
+						<table width="200px">
+							
+							<tr>
+								<td><b>Superior Room	 </b></td>
+								<td><button type="button" class="btn btn-primary btn-circle"><?php  
+									$f1 =$sc - $csc;
+									if($f1 <=0 )
+									{	$f1 = "NO";
+										echo $f1;
+									}
+									else{
+											echo $f1;
+									}
+								
+								
+								?> </button></td> 
+							</tr>
+							<tr>
+								<td><b>Guest House</b>	 </td>
+								<td><button type="button" class="btn btn-primary btn-circle"><?php 
+								$f2 =  $gh -$cgh;
+								if($f2 <=0 )
+									{	$f2 = "NO";
+										echo $f2;
+									}
+									else{
+											echo $f2;
+									}
+
+								?> </button></td> 
+							</tr>
+							<tr>
+								<td><b>Single Room	 </b></td>
+								<td><button type="button" class="btn btn-primary btn-circle"><?php
+								$f3 =$sr - $csr;
+								if($f3 <=0 )
+									{	$f3 = "NO";
+										echo $f3;
+									}
+									else{
+											echo $f3;
+									}
+
+								?> </button></td> 
+							</tr>
+							<tr>
+								<td><b>Deluxe Room</b>	 </td>
+								<td><button type="button" class="btn btn-primary btn-circle"><?php 
+								
+								$f4 =$dr - $cdr; 
+								if($f4 <=0 )
+									{	$f4 = "NO";
+										echo $f4;
+									}
+									else{
+											echo $f4;
+									}
+								?> </button></td> 
+							</tr>
+							<tr>
+								<td><b>Total Rooms	</b> </td>
+								<td> <button type="button" class="btn btn-danger btn-circle"><?php 
+								
+								$f5 =$r-$cr; 
+								if($f5 <=0 )
+									{	$f5 = "NO";
+										echo $f5;
+									}
+									else{
+											echo $f5;
+									}
+								 ?> </button></td> 
+							</tr>
+						</table>
+						
+						
+						
+                        
+						
+						</div>
+                        <div class="panel-footer">
+                            
+                        </div>
+                    </div>
+					</div>
+                </div>
+                <!-- /. ROW  -->
+				
+                </div>
+                <!-- /. ROW  -->
+				
+				
+				
+				
+         </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+    <!-- /. WRAPPER  -->
+    <!-- JS Scripts-->
+    <!-- jQuery Js -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- Bootstrap Js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Metis Menu Js -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+    <!-- Morris Chart Js -->
+    <script src="assets/js/morris/raphael-2.1.0.min.js"></script>
+    <script src="assets/js/morris/morris.js"></script>
+    <!-- Custom Js -->
+    <script src="assets/js/custom-scripts.js"></script>
+
+
+</body>
+
+</html>
+
+<?php
+						if(isset($_POST['co']))
+						{	
+							$st = $_POST['conf'];
+							
+							 
+							
+							if($st=="Conform")
+							{
+									$urb = "UPDATE `roombook` SET `stat`='$st' WHERE id = '$id'";
+									
+								if($f1=="NO" )
+								{
+									echo "<script type='text/javascript'> alert('Sorry! Not Available Superior Room ')</script>";
+								}
+								else if($f2 =="NO")
+									{
+										echo "<script type='text/javascript'> alert('Sorry! Not Available Guest House')</script>";
+										
+									}
+									else if ($f3 == "NO")
+									{
+										echo "<script type='text/javascript'> alert('Sorry! Not Available Single Room')</script>";
+									}
+										else if($f4=="NO")
+										{
+										echo "<script type='text/javascript'> alert('Sorry! Not Available Deluxe Room')</script>";
+										}
+										
+										else if( mysqli_query($con,$urb))
+											{	
+												//echo "<script type='text/javascript'> alert('Guest Room booking is conform')</script>";
+												//echo "<script type='text/javascript'> window.location='home.php'</script>";
+												 $type_of_room = 0;       
+														if($troom=="Superior Room")
+														{
+															$type_of_room = 320;
+														
+														}
+														else if($troom=="Deluxe Room")
+														{
+															$type_of_room = 220;
+														}
+														else if($troom=="Guest House")
+														{
+															$type_of_room = 180;
+														}
+														else if($troom=="Single Room")
+														{
+															$type_of_room = 150;
+														}
+														
+														
+														
+														
+														if($bed=="Single")
+														{
+															$type_of_bed = $type_of_room * 1/100;
+														}
+														else if($bed=="Double")
+														{
+															$type_of_bed = $type_of_room * 2/100;
+														}
+														else if($bed=="Triple")
+														{
+															$type_of_bed = $type_of_room * 3/100;
+														}
+														else if($bed=="Quad")
+														{
+															$type_of_bed = $type_of_room * 4/100;
+														}
+														else if($bed=="None")
+														{
+															$type_of_bed = $type_of_room * 0/100;
+														}
+														
+														
+														if($meal=="Room only")
+														{
+															$type_of_meal=$type_of_bed * 0;
+														}
+														else if($meal=="Breakfast")
+														{
+															$type_of_meal=$type_of_bed * 2;
+														}else if($meal=="Half Board")
+														{
+															$type_of_meal=$type_of_bed * 3;
+														
+														}else if($meal=="Full Board")
+														{
+															$type_of_meal=$type_of_bed * 4;
+														}
+														
+														
+														$ttot = $type_of_room * $days * $nroom;
+														$mepr = $type_of_meal * $days;
+														$btot = $type_of_bed *$days;
+														
+														$fintot = $ttot + $mepr + $btot ;
+															
+															//echo "<script type='text/javascript'> alert('$count_date')</script>";
+														$psql = "INSERT INTO `payment`(`id`, `title`, `fname`, `lname`, `troom`, `tbed`, `nroom`, `cin`, `cout`, `ttot`,`meal`, `mepr`, `btot`,`fintot`,`noofdays`) VALUES ('$id','$title','$fname','$lname','$troom','$bed','$nroom','$cin','$cout','$ttot','$meal','$mepr','$btot','$fintot','$days')";
+														
+														if(mysqli_query($con,$psql))
+														{	$notfree="NotFree";
+															$rpsql = "UPDATE `room` SET `place`='$notfree',`cusid`='$id' where bedding ='$bed' and type='$troom' ";
+															if(mysqli_query($con,$rpsql))
+															{
+															echo "<script type='text/javascript'> alert('Booking Conform')</script>";
+															echo "<script type='text/javascript'> window.location='roombook.php'</script>";
+															}
+															
+															
+														}
+												
+											}
+									
+                                        
+							}	
+					
+						}
+					
+									
+									
+							
+						?>
