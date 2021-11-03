9682734726846c64d9b0e565947bf67166110530
QuangAnh17920 <67898698+QuangAnh17920@users.noreply.github.com>
2021-11-04 02:23:08 +0700

    Update reservation.php

--- admin/reservation.php
+++ admin/reservation.php
@@ -187,3 +187,82 @@ include('db.php')
                         
                     </div>
                 </div>
+                <div class="col-md-12 col-sm-12">
+                    <div class="well">
+                        <h4>HUMAN VERIFICATION</h4>
+                        <p>Type Below this code <?php $Random_code=rand(); echo$Random_code; ?> </p><br />
+						<p>Enter the random code<br /></p>
+							<input  type="text" name="code1" title="random code" />
+							<input type="hidden" name="code" value="<?php echo $Random_code; ?>" />
+						<input type="submit" name="submit" class="btn btn-primary">
+						<?php
+							if(isset($_POST['submit']))
+							{
+							$code1=$_POST['code1'];
+							$code=$_POST['code']; 
+							if($code1!="$code")
+							{
+							$msg="Invalide code"; 
+							}
+							else
+							{
+							
+									$con=mysqli_connect("localhost","root","","hotel");
+									$check="SELECT * FROM roombook WHERE email = '$_POST[email]'";
+									$rs = mysqli_query($con,$check);
+									$data = mysqli_fetch_array($rs, MYSQLI_NUM);
+									if($data[0] > 1) {
+										echo "<script type='text/javascript'> alert('User Already in Exists')</script>";
+										
+									}
+
+									else
+									{
+										$new ="Not Conform";
+										$newUser="INSERT INTO `roombook`(`Title`, `FName`, `LName`, `Email`, `National`, `Country`, `Phone`, `TRoom`, `Bed`, `NRoom`, `Meal`, `cin`, `cout`,`stat`,`nodays`) VALUES ('$_POST[title]','$_POST[fname]','$_POST[lname]','$_POST[email]','$_POST[nation]','$_POST[country]','$_POST[phone]','$_POST[troom]','$_POST[bed]','$_POST[nroom]','$_POST[meal]','$_POST[cin]','$_POST[cout]','$new',datediff('$_POST[cout]','$_POST[cin]'))";
+										if (mysqli_query($con,$newUser))
+										{
+											echo "<script type='text/javascript'> alert('Your Booking application has been sent')</script>";
+											
+										}
+										else
+										{
+											echo "<script type='text/javascript'> alert('Error adding user in database')</script>";
+											
+										}
+									}
+
+							$msg="Your code is correct";
+							}
+							}
+							?>
+						</form>
+							
+                    </div>
+                </div>
+            </div>
+           
+                
+                </div>
+                    
+            
+				
+					</div>
+			 <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+    <!-- JS Scripts-->
+    <!-- jQuery Js -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- Bootstrap Js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Metis Menu Js -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+      <!-- Custom Js -->
+    <script src="assets/js/custom-scripts.js"></script>
+    
+   
+</body>
+</html>
