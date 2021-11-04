2a5d570dea4fbfd80011e02101cf8e110d142cde
Ngotienthanh <67349476+Ngotienthanh@users.noreply.github.com>
2021-11-04 11:02:29 +0700

    JS

--- admin/room.php
+++ admin/room.php
@@ -217,5 +217,16 @@ if(!isset($_SESSION["user"]))
         </div>
          <!-- /. PAGE WRAPPER  -->
  </div>
+	
+	<!-- JS Scripts-->
+    
+    <!-- jQuery Js -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- Bootstrap Js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Metis Menu Js -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+      <!-- Custom Js -->
+    <script src="assets/js/custom-scripts.js"></script>
 </body>
 </html>
