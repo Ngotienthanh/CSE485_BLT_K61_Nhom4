b1332c41b21df4f99c778833818c2b3db39ec6cb
Ngotienthanh <67349476+Ngotienthanh@users.noreply.github.com>
2021-11-05 23:25:20 +0700

    Masterpage Layout Index page

---
+++ masterpage_layout/about.php
@@ -0,0 +1,48 @@
+<div class="about-wthree" id="about">
+	<div class="container">
+		<div class="ab-w3l-spa">
+			<h3 class="title-w3-agileits title-black-wthree">About Our SUN RISE</h3> 
+			<p class="about-para-w3ls">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Sed tempus vestibulum lacus blandit faucibus. Nunc imperdiet, diam nec rhoncus ullamcorper, nisl nulla suscipit ligula, at imperdiet urna</p>
+			<img src="images/about.jpg" class="img-responsive" alt="Hair Salon">
+			<div class="w3l-slider-img">
+				<img src="images/a1.jpg" class="img-responsive" alt="Hair Salon">
+			</div>
+			<div class="w3ls-info-about">
+				<h4>You'll love all the amenities we offer!</h4>
+				<p>Lorem ipsum dolor sit amet, ut magna aliqua. </p>
+			</div>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+</div>
+
+<!--sevices-->
+<div class="advantages">
+	<div class="container">
+		<div class="advantages-main">
+			<h3 class="title-w3-agileits">Our Services</h3>
+			<div class="advantage-bottom">
+				<div class="col-md-6 advantage-grid left-w3ls wow bounceInLeft" data-wow-delay="0.3s">
+					<div class="advantage-block ">
+						<i class="fa fa-credit-card" aria-hidden="true"></i>
+						<h4>Stay First, Pay After! </h4>
+						<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates.</p>
+						<p><i class="fa fa-check" aria-hidden="true"></i>Decorated room, proper air conditioned</p>
+						<p><i class="fa fa-check" aria-hidden="true"></i>Private balcony</p>
+					</div>
+				</div>
+				<div class="col-md-6 advantage-grid right-w3ls wow zoomIn" data-wow-delay="0.3s">
+					<div class="advantage-block">
+						<i class="fa fa-clock-o" aria-hidden="true"></i>
+						<h4>24 Hour Restaurant</h4>
+						<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates.</p>
+						<p><i class="fa fa-check" aria-hidden="true"></i>24 hours room service</p>
+						<p><i class="fa fa-check" aria-hidden="true"></i>24-hour Concierge service</p>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--//sevices-->
---
+++ masterpage_layout/banner.php
@@ -0,0 +1,63 @@
+<!-- //Modal1 -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog">
+	<!-- Modal1 -->
+	<div class="modal-dialog">
+		<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4>SUN  <span>RISE</span></h4>
+				<img src="images/1.jpg" alt=" " class="img-responsive">
+				<h5>We know what you love</h5>
+				<p>Providing guests unique and enchanting views from their rooms with its exceptional amenities, makes Star Hotel one of bests in its kind.Try our food menu, awesome services and friendly staff while you are here.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- //Modal1 -->
+<div id="availability-agileits">
+	<div class="col-md-12 book-form-left-w3layouts">
+		<a href="admin/reservation.php"><h2>ROOM RESERVATION</h2></a>
+	</div>
+	<div class="clearfix"> </div>
+</div>
+<!-- banner-bottom -->
+<div class="banner-bottom">
+	<div class="container">	
+		<div class="agileits_banner_bottom">
+			<h3><span>Experience a good stay, enjoy fantastic offers</span> Find our friendly welcoming reception</h3>
+		</div>
+		<div class="w3ls_banner_bottom_grids">
+			<ul class="cbp-ig-grid">
+				<li>
+					<div class="w3_grid_effect">
+						<span class="cbp-ig-icon w3_road"></span>
+						<h4 class="cbp-ig-title">MASTER BEDROOMS</h4>
+						<span class="cbp-ig-category">SUN RISE</span>
+					</div>
+				</li>
+				<li>
+					<div class="w3_grid_effect">
+						<span class="cbp-ig-icon w3_cube"></span>
+						<h4 class="cbp-ig-title">SEA VIEW BALCONY</h4>
+						<span class="cbp-ig-category">SUN RISE</span>
+					</div>
+				</li>
+				<li>
+					<div class="w3_grid_effect">
+						<span class="cbp-ig-icon w3_users"></span>
+						<h4 class="cbp-ig-title">LARGE CAFE</h4>
+						<span class="cbp-ig-category">SUN RISE</span>
+					</div>
+				</li>
+				<li>
+					<div class="w3_grid_effect">
+						<span class="cbp-ig-icon w3_ticket"></span>
+						<h4 class="cbp-ig-title">WIFI COVERAGE</h4>
+						<span class="cbp-ig-category">SUN RISE</span>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
