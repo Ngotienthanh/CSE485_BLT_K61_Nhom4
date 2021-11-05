9a903b0f069a54da3e62db850e80caaba36a3c7a
Ngotienthanh <67349476+Ngotienthanh@users.noreply.github.com>
2021-11-05 10:28:26 +0700

    Update

--- admin/roomdel.php
+++ admin/roomdel.php
@@ -78,7 +78,147 @@ $rre=mysqli_query($con,$rsql);
 				</ul>
             </div>
         </nav>
-	 
+	<!-- /. NAV SIDE  -->
+        <div id="page-wrapper" >
+            <div id="page-inner">
+				<div class="row">
+					<div class="col-md-12">
+						<h1 class="page-header">
+							DELETE ROOM <small></small>
+						</h1>
+					</div>
+				</div>             
+				<div class="row">
+					<div class="col-md-12 col-sm-12">
+						<div class="panel panel-primary">
+							<div class="panel-heading">Delete room</div>
+							<div class="panel-body">
+							<form name="form" method="post">
+								<div class="form-group">
+									<label>Select the Room ID *</label>
+									<select name="id"  class="form-control" required>
+										<option value selected ></option>
+										<?php
+										while($rrow=mysqli_fetch_array($rre))
+										{
+										$value = $rrow['id'];
+											echo '<option value="'.$value.'">'.$value.'</option>';
+										
+										}
+										?>
+										
+									</select>
+								</div>
+								<input type="submit" name="del" value="Delete Room" class="btn btn-primary"> 
+							</form>
+								<?php
+								include('db.php');
+								
+								if(isset($_POST['del']))
+								{
+									$did = $_POST['id'];
+									
+									
+									$sql ="DELETE FROM `room` WHERE id = '$did'" ;
+									if(mysqli_query($con,$sql))
+									{
+									echo '<script type="text/javascript">alert("Delete the Room") </script>' ;
+											
+											header("Location: roomdel.php");
+									}else {
+										echo '<script>alert("Sorry ! Check The System") </script>' ;
+									}
+								}
+								
+								?>
+						</div>
+							
+					</div>
+				</div>
+                
+                  
+			<?php
+					include ('db.php');
+					$sql = "select * from room";
+					$re = mysqli_query($con,$sql)
+				?>
+				<div class="row">
+					<?php
+						while($row= mysqli_fetch_array($re))
+						{
+								$id = $row['type'];
+							if($id == "Superior Room") 
+							{
+								echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+									<div class='panel panel-primary text-center no-boder bg-color-blue'>
+										<div class='panel-body'>
+											<i class='fa fa-users fa-5x'></i>
+											<h3>".$row['bedding']."</h3>
+										</div>
+										<div class='panel-footer back-footer-blue'>
+											".$row['type']."
+
+										</div>
+									</div>
+								</div>";
+							}
+							else if ($id == "Deluxe Room")
+							{
+								echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+									<div class='panel panel-primary text-center no-boder bg-color-green'>
+										<div class='panel-body'>
+											<i class='fa fa-users fa-5x'></i>
+											<h3>".$row['bedding']."</h3>
+										</div>
+										<div class='panel-footer back-footer-green'>
+											".$row['type']."
+
+										</div>
+									</div>
+								</div>";
+							
+							}
+							else if($id =="Guest House")
+							{
+								echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+									<div class='panel panel-primary text-center no-boder bg-color-brown'>
+										<div class='panel-body'>
+											<i class='fa fa-users fa-5x'></i>
+											<h3>".$row['bedding']."</h3>
+										</div>
+										<div class='panel-footer back-footer-brown'>
+											".$row['type']."
+
+										</div>
+									</div>
+								</div>";
+							
+							}
+							else if($id =="Single Room")
+							{
+								echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+									<div class='panel panel-primary text-center no-boder bg-color-red'>
+										<div class='panel-body'>
+											<i class='fa fa-users fa-5x'></i>
+											<h3>".$row['bedding']."</h3>
+										</div>
+										<div class='panel-footer back-footer-red'>
+											".$row['type']."
+
+										</div>
+									</div>
+								</div>";
+							}
+						}
+					?> 
+				</div>
+
+				<?php ob_end_flush(); ?>
+
+			</div>
+			<!-- /. PAGE INNER  -->
+		</div>
+		<!-- /. PAGE WRAPPER  -->
 </div>
  
      <!-- JS Scripts-->
